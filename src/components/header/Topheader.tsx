5c6183b72bde761b19c9d5d88aa35e1cb626199a
sohelkhan <mdsohelkhan418126@gmail.com>
2026-09-22 22:54:55 +0600

    Remove unused SVG files and update global styles; modify metadata and restructure main page layout

---
+++ src/components/header/Topheader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Topheader = () => {
+  return (
+    <div className="bg-[#010f1c] text-gray-200">
+      <div className=" container mx-auto px-4 py-2">
+        <p> FREE Express Shipping On Orders $1000+</p>
+      </div>
+    </div>
+  );
+};
+
+export default Topheader;
